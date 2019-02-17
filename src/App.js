0fd87e174238b158539310bdc69e58b73a9c7aa1
James Eby <temporafugiunt@gmail.com>
2019-02-16 22:45:38 -0600

    Added prettier and eslint support.

--- src/App.js
+++ src/App.js
@@ -1,37 +1,39 @@
+/* global React ReactDOM */
+
 // Function components are good when a component doean't need state or
 // other lifecycle functions built in react.
 const Pet = (props) => {
-    return React.createElement("div", {}, [
-        React.createElement("h1", {}, props.name),
-        React.createElement("h2", {}, props.animal),
-        React.createElement("h2", {}, props.breed)
-    ]);
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h2", {}, props.breed),
+  ]);
 };
 
 class App extends React.Component {
-    handleTitleClick() {
-        alert("You clicked the title!")
-    }
-    render() {
-        return React.createElement("div", {}, [
-            React.createElement("h1", { onClick: this.handleTitleClick }, "Adopt a Pet!"),
-            React.createElement(Pet, {
-                name: "Rudy",
-                animal: "Dog",
-                breed: "Dachshund"
-            }),
-            React.createElement(Pet, {
-                name: "Katie",
-                animal: "Dog",
-                breed: "Dachshund"
-            }),
-            React.createElement(Pet, {
-                name: "Corkie",
-                animal: "Dog",
-                breed: "Shnoodle"
-            })
-        ]);
-    }
+  handleTitleClick() {
+    alert("You clicked the title!");
+  }
+  render() {
+    return React.createElement("div", {}, [
+      React.createElement("h1", { onClick: this.handleTitleClick }, "Adopt a Pet!"),
+      React.createElement(Pet, {
+        name: "Rudy",
+        animal: "Dog",
+        breed: "Dachshund",
+      }),
+      React.createElement(Pet, {
+        name: "Katie",
+        animal: "Dog",
+        breed: "Dachshund",
+      }),
+      React.createElement(Pet, {
+        name: "Corkie",
+        animal: "Dog",
+        breed: "Shnoodle",
+      }),
+    ]);
+  }
 }
 
 ReactDOM.render(React.createElement(App), document.getElementById("root"));
