b53197c618495d6b5223995f43754f3692ea1bf5
James Eby <temporafugiunt@gmail.com>
2019-02-17 19:41:05 -0600

    Checkin before change over to RESTful data.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,39 @@
 import React from "react";
 import { render } from "react-dom";
+import pf from "petfinder-client";
 import Pet from "./Pet";
 
+const petfinder = pf({
+  key: process.env.API_KEY,
+  secret: process.env.API_SECRET,
+});
+
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      pets: [],
+    };
+  }
+
+  componentDidMount() {
+    petfinder.pet.find({ output: "full", location: "Rockrord, IL" }).then((data) => {
+      let pets;
+
+      if (data.petfinder.pets && data.petfinder.pets.pet) {
+        if (Array.isArray(data.petfinder.pets.pet)) {
+          pets = data.petfinder.pets.pet;
+        } else {
+          pets = [data.petfinder.pets.pet];
+        }
+      } else {
+        pets = [];
+      }
+
+      this.setState({ pets: pets });
+    });
+  }
   render() {
     return (
       <React.Fragment>
