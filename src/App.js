5dfd23d39f97a3dfc5f31daecbb8556a7fc75730
James Eby <temporafugiunt@gmail.com>
2019-02-17 21:56:54 -0600

    Before transformation removing constructors for newer javascript standard.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React from "react";
 import { render } from "react-dom";
+import { Router, Link } from "@reach/router";
+
 import Results from "./Results";
+import Details from "./Details";
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>Adopt Me!</h1>
-        <div>
-          <Results />
-        </div>
+        <header>
+          <Link to="/">Adopt Me!</Link>
+        </header>
+        <Router>
+          <Results path="/" />
+          <Details path="/details/:id" />
+        </Router>
       </div>
     );
   }
